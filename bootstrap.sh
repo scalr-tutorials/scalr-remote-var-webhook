53e71eadb26ee4dc718b55c15fd9c4c75a9ac555
lcc2207 <lynn@fidmail.com>
2019-01-15 14:23:03 -0600

    add mysql example

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-apt update
-apt install python-pip -y
+
+OS=$(cat /etc/os-release | grep "PRETTY_NAME" | sed 's/PRETTY_NAME=//g' | sed 's/["]//g' | awk '{print $1}')
+
+if [ $OS == "CentOS" ]
+then
+  yum update -y
+  yum install epel-release -y
+  yum install python-pip git unzip -y
+elif [ $OS == "Ubuntu" ]
+then
+  apt update
+  apt install python-pip -y
+fi
+
+appfolder=/opt/variable-webhook/
 
 pip install docker-compose
 
@@ -9,4 +22,4 @@ service docker start || systemctl start docker
 
 mkdir -p /var/log/webhook
 
-git clone https://github.com/scalr-tutorials/scalr-remote-var-webhook.git /opt/variable-webhook
+git clone https://github.com/scalr-tutorials/scalr-remote-var-webhook.git $appfolder
--- webhook_mysql.py
+++ webhook_mysql.py
@@ -22,17 +22,27 @@ app = Flask(__name__)
 # Configuration variables
 SCALR_SIGNING_KEY = os.getenv('SCALR_SIGNING_KEY', '')
 SCALR_WEBHOOK = os.getenv('SCALR_WEBHOOK', '')
-SCALR_JSONFILE = os.getenv('SCALR_JSONFILE')
+SCALR_MYSQL_SERVER = os.getenv('SCALR_MYSQL_SERVER')
+SCALR_MYSQL_USER = os.getenv('SCALR_MYSQL_USER')
+SCALR_MYSQL_PASS = os.getenv('SCALR_MYSQL_PASS')
+SCALR_MYSQL_DB = os.getenv('SCALR_MYSQL_DB')
+
 
 for var in ['SCALR_SIGNING_KEY', 'SCALR_WEBHOOK']:
     logging.info('Config: %s = %s', var, globals()[var] if 'PASS' not in var else '*' * len(globals()[var]))
 
 @app.route('/' + SCALR_WEBHOOK + '/', methods=['POST', 'GET'])
 def webhook_listener():
-    logging.info(request)
     if not validate_request(request):
         abort(403)
-    out=json.load(open(SCALR_JSONFILE))
+
+    mydb = mysql.connector.connect(host=SCALR_MYSQL_SERVER, user=SCALR_MYSQL_USER, passwd=SCALR_MYSQL_PASS, database=SCALR_MYSQL_DB)
+    var = mydb.cursor()
+    var.execute("SELECT * FROM variables")
+    json_data=[]
+    for (name) in var:
+        json_data.append({"value": name[0]})
+    out={"list": json_data,"ttl": 3 }
     logging.info(out)
     return jsonify(out)
 
